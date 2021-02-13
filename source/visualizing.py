7e1c4e34c5591648629aa2064a27ab7132d11b5a
TomislavZupanovic <tzupan01@fesb.hr>
2021-02-13 12:54:05 +0100

    Update

--- source/visualizing.py
+++ source/visualizing.py
@@ -22,18 +22,20 @@ class Visualizer:
         plt.show()
 
     @staticmethod
+    def plot_lines(idx, pred, ax):
+        """ Plots lines between key points """
+        points = [0, 17, 22, 27, 31, 36, 42, 48, 60]
+        for p in range(len(points) - 1):
+            j = p + 1
+            ax[idx].plot(pred[points[p]:points[j], 0], pred[points[p]:points[j], 1], linestyle='solid', c='red')
+        # Extra line plots to close holes in key points
+        ax[idx].plot([pred[36, 0], pred[41, 0]], [pred[36, 1], pred[41, 1]], linestyle='solid', c='red')
+        ax[idx].plot([pred[42, 0], pred[47, 0]], [pred[42, 1], pred[47, 1]], linestyle='solid', c='red')
+        ax[idx].plot([pred[48, 0], pred[59, 0]], [pred[48, 1], pred[59, 1]], linestyle='solid', c='red')
+
+    @staticmethod
     def visualize_output(images, pred_keypoints, lines=False, keypoints=None):
         """ Visualizes the model predicted key points on given images """
-        def plot_lines(idx):
-            """ Plots lines between key points """
-            points = [0, 17, 22, 27, 31, 36, 42, 48, 60]
-            for p in range(len(points) - 1):
-                j = p + 1
-                ax[idx].plot(pred[points[p]:points[j], 0], pred[points[p]:points[j], 1], linestyle='solid', c='red')
-            # Extra line plots to close holes in key points
-            ax[idx].plot([pred[36, 0], pred[41, 0]], [pred[36, 1], pred[41, 1]], linestyle='solid', c='red')
-            ax[idx].plot([pred[42, 0], pred[47, 0]], [pred[42, 1], pred[47, 1]], linestyle='solid', c='red')
-            ax[idx].plot([pred[48, 0], pred[59, 0]], [pred[48, 1], pred[59, 1]], linestyle='solid', c='red')
         # Define figure and subplots
         figure, ax = plt.subplots(1, 4, figsize=(10, 5))
         figure.suptitle('Model predictions', fontsize=15)
@@ -51,7 +53,7 @@ class Visualizer:
             if not lines:
                 ax[i].scatter(pred[:, 0], pred[:, 1], s=5, marker='.', c='red')
             elif lines:
-                plot_lines(i)
+                Visualizer.plot_lines(i, pred, ax)
             if keypoints is not None:
                 real = keypoints[i]
                 real = real * 50.0 + 100
